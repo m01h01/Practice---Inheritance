d781aaf1756cddf5882dea3f87bf727ce83caf24
m01h01 <mheang01@student.rtc.edu>
2023-04-19 23:44:03 -0700

    Initial commit

---
+++ Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice___Inheritance
+{
+    internal class Vehicle
+    {
+        string _color;
+
+        public Vehicle(string color)
+        {
+            Color = color;
+        }
+
+        public string Color { get => _color; set => _color = value; }
+
+        public override string ToString() 
+        {
+            return this.GetType() + $" - Color: {_color}"; 
+        }
+    }//class
+}//namespace
